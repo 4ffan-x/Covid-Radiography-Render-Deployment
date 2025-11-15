17c9669ef495483e83701ad052b0be258783787d
affandhanbad-dev <affandhanbad@gmail.com>
2025-11-15 16:00:03 +0530

    fixing port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect, url_for, send_from_directory
+import os
 import cv2
 import numpy as np
 import os
@@ -58,4 +59,5 @@ def upload_file():
                                confidence_score=confidence_score)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
